ca82e60343347992135c95075806b1c5da5ba6a2
Fredy Nurman Saleh <email@fredyns.net>
2025-09-01 21:53:00 +0700

    show Record: add icon to buttons

--- resources/views/livewire/sample/records/show.blade.php
+++ resources/views/livewire/sample/records/show.blade.php
@@ -7,20 +7,27 @@ use App\Models\Sample\SampleRecord;
 
 ?>
 
-<section class="w-full max-w-md mx-auto px-4 sm:px-6 lg:px-8">
+<section class="w-full max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
 
     <div class="relative mb-6 w-full">
         <flux:heading size="xl" level="1">{{ __('View Record') }}</flux:heading>
         <flux:separator variant="subtle"/>
     </div>
 
-    <div class="flex flex-grow gap-x-4 mb-4">
-        <flux:button href="{{ route('sample.records.index') }}" variant="filled">{{ __('Index') }}</flux:button>
+    <div class="flex flex-grow gap-x-4 mb-4 justify-end">
+        <flux:button
+            href="{{ route('sample.records.index') }}"
+            variant="filled"
+            icon="table-cells"
+        >
+            {{ __('Index') }}
+        </flux:button>
         {{--        <flux:button href="{{ route('sample.records.edit', $record) }}" variant="filled">{{ __('Edit') }}</flux:button>--}}
         <flux:button
             wire:confirm="Are you sure?"
             wire:click="delete()"
             variant="danger" type="button"
+            icon="trash"
         >
             {{ __('Delete') }}
         </flux:button>
